3aa7610a3e3bf6d3c43a25b23bdc00cd522b976a
Abdul Samad <sheikh.samad.dev@gmail.com>
2026-07-31 22:57:57 +0530

    logical operator

--- 03_operators/operators.py
+++ 03_operators/operators.py
@@ -16,3 +16,24 @@ print(x==y)
 print(x!=y)
 print(x<y)
 print(x>=y)
+print("___________________________________________________")
+
+
+# Logical operations
+
+#AND operator
+age = 20 
+citizen = True
+if age >= 18 and citizen:
+    print("Eligible")
+
+# OR operator
+mark = 85
+sport = True
+if mark >= 90 or sport:
+    print("selected")
+
+# NOT operator
+logged_in = False
+if not logged_in:
+    print("Please login ")
