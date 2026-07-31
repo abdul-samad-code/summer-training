e28787fa71668cc45c43c547e7c3c7952250dae8
Abdul Samad <sheikh.samad.dev@gmail.com>
2026-07-31 22:24:35 +0530

    comparison operator

---
+++ 03_operators/operators.py
@@ -0,0 +1,18 @@
+#  Comparison Operators
+
+"""
+==  equal        a==b
+!=  NOt equal    a!=b
+>   Greater than a>b
+<   less than    a<b
+<=
+>=
+"""
+
+# example
+x = 10
+y = 20
+print(x==y)
+print(x!=y)
+print(x<y)
+print(x>=y)
