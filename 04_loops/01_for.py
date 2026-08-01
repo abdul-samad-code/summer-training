bdf2554a3ad8c13df3cef976b00c3992201e34bb
Abdul Samad <sheikh.samad.dev@gmail.com>
2026-08-01 22:47:15 +0530

    for loop

---
+++ 04_loops/01_for.py
@@ -0,0 +1,15 @@
+# loops :- for loop ka use kisi sequence (list, string, range, etc.) ke har element par iterate karne ke liye hota hai.
+
+# for :- for loop ka use kisi sequence (list, string, range, etc.) ke har element par iterate karne ke liye hota hai.
+
+# Example 1
+for i in range(5):
+    print(i)
+
+# Example 2
+for i in range(1, 6):
+    print(i)
+
+# Example 3
+for i in range(0, 11, 2):
+    print(i)
