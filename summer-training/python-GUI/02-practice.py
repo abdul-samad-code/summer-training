b02ee6aa811a2c2a3dc7b322136b356f9497f4da
Abdul Samad <sheikh.samad.dev@gmail.com>
2026-08-08 23:45:01 +0530

    typo

---
+++ summer-training/python-GUI/02-practice.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import ttk 
+
+root = tk.Tk()
+root.geometry("800x400+50+50")
+
+root.mainloop()
--- summer-training/python-GUI/demo.py
+++ summer-training/python-GUI/demo.py
@@ -3,9 +3,11 @@
 import tkinter as tk
 root = tk.Tk()       # Tk main /root window create karta hai
 root.title("My Application")
+
 root.geometry("500x300")
 root.configure(bg="lightblue")    # bg color 
-root.resizable(false, false)
+root.resizabl
+e(True, false)
 root.mainloop()      # it opens the window and wait for user action 
 
 
