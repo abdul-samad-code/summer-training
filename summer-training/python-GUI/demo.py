56cc1775cd42281741852e3e4a1871cfbc3a7e13
Abdul Samad <sheikh.samad.dev@gmail.com>
2026-08-08 23:52:49 +0530

    center window

--- summer-training/python-GUI/demo.py
+++ summer-training/python-GUI/demo.py
@@ -3,11 +3,20 @@
 import tkinter as tk
 root = tk.Tk()       # Tk main /root window create karta hai
 root.title("My Application")
+width = 500
+height = 300
 
-root.geometry("500x300")
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x = (screen_width - width) // 2
+y = (screen_height - height) //2
+root.geometry(f"{width}x{height}+{x}+{y}")
+
+
 root.configure(bg="lightblue")    # bg color 
-root.resizabl
-e(True, false)
+root.resizable(True, False)
+
 root.mainloop()      # it opens the window and wait for user action 
 
 
