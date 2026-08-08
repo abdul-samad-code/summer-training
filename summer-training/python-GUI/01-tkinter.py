c9ce38e1dd77002edbbd21dd356d3594c2b0347a
Abdul Samad <sheikh.samad.dev@gmail.com>
2026-08-08 20:50:39 +0530

    labels and button in tkinter

---
+++ summer-training/python-GUI/01-tkinter.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+def newWindow():
+    newWindow = tk.Toplevel(root)
+    newWindow.title("new window")
+    newWindow.geometry("400x300")
+    tk.Label(root, text="this is new window").pack()
+    ttk.Button(newWindow, text="close", command=newWindow).pack()
+
+tk.Label(root, text="classic label",).pack()
+ttk.Label(root, text="themed label").pack()
+"""
+buttonName = ttk.Button(
+root,
+master,
+text="text"),
+command=callback
+"""
+button = tk.Button(root, text="classic button",).pack()
+button2 = ttk.Button(root, text="themed button",
+                    command=lambda: print("themed button clicked")).pack()
+button2.state(['disabled'])
+button3 = ttk.Button(root, text="open new window",
+ command=newWindow).pack()
+root.mainloop()
+
--- summer-training/python-GUI/demo.py
+++ summer-training/python-GUI/demo.py
@@ -3,6 +3,9 @@
 import tkinter as tk
 root = tk.Tk()       # Tk main /root window create karta hai
 root.title("My Application")
+root.geometry("500x300")
+root.configure(bg="lightblue")    # bg color 
+root.resizable(false, false)
 root.mainloop()      # it opens the window and wait for user action 
 
 
