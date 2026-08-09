af38928849c1631b1ad8cb36652b0e65649e71a9
Abdul Samad <sheikh.samad.dev@gmail.com>
2026-08-09 19:02:27 +0530

    transparent window

---
+++ summer-training/python-GUI/04-transparency.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+root = tk.Tk()
+root.title('tkinter window demo')
+root.geometry('600x400+50+50')
+root.resizable(False, False)
+root.attributes('-alpha', 0.5)
+root.mainloop()
